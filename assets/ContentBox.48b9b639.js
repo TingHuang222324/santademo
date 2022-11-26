3e14ee8df532f61ca3c1afebd5bc3ac601d41d8f
TingHuang222324 <t99635@gmail.com>
2022-11-26 11:27:38 +0800

    history mode

--- assets/ContentBox.48b9b639.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,a as n,e as o,r as _}from"./index.0363f4c0.js";const s={},a={class:"content-box bg-light"};function c(e,r){return n(),o("div",a,[_(e.$slots,"default",{},void 0,!0)])}const l=t(s,[["render",c],["__scopeId","data-v-17a9a8e4"]]);export{l as C};
